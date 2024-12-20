6f94b605152a05917fea2ea3f771bf6247247469
Shubham Nawani <62939406+shubhamnawani99@users.noreply.github.com>
2024-12-20 08:09:38 +0000

    22. DOM Manipulation

---
+++ 06_dom/index.js
@@ -0,0 +1,4 @@
+// Easy 3 steps to change select any element(s) and change their properties
+document.querySelectorAll('h3') 
+h3 = document.querySelectorAll('h3') 
+h3.forEach((element) => element.style.color = 'Red') 
